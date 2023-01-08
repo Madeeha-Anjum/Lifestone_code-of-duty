7f06c3a7450dd871f441c020263643307315d3fb
Ansh Dubey <ansh@ualberta.ca>
2023-01-07 23:42:27 -0700

    modifications needed

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 from routers import user, milestones
 import models
 from database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Creates the database tables if they don't exist
 models.Base.metadata.create_all(bind=engine)
